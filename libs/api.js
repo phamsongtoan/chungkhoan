ed5fcc6163fdc854ebdb642c80d7232b9935a315
Thanh Tam <thanhtam.v2t@gmail.com>
2023-10-21 01:27:19 +0700

    s

--- libs/api.js
+++ libs/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_API || 'https://apichungkhoang.orokucode.com/'
+  baseURL: process.env.NEXT_API || 'https://apichungkhoang.orokucode.com/api'
 })
