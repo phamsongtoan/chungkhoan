8172e8c7de255debd0f0cf77684ebcc5a8b806cf
Thanh Tam <thanhtam.v2t@gmail.com>
2023-11-11 23:48:09 +0700

    test

--- Components/ComponentsInner/Image/index.js
+++ Components/ComponentsInner/Image/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ImageA = ({ src }) => {
   return (
-    // <img src={`https://apichungkhoang.orokucode.com${src}`} alt='cho co phieu'/>
-    <img src={`http://localhost:1337${src}`} alt='cho co phieu'/>
+    <img src={`https://apichungkhoang.orokucode.com${src}`} alt='cho co phieu'/>
+  // <img src={`http://localhost:1337${src}`} alt='cho co phieu'/>
 
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,8 @@ import 'react-image-lightbox/style.css'
 import { useEffect } from 'react'
 import { api } from '../libs/api'
 import { AppProvider } from '../Context/AppContext'
+import Head from 'next/head'
+import Script from 'next/script'
 
 function MyApp ({ Component, pageProps, ...props }) {
   console.log('🐳 -> MyApp -> props', props)
@@ -24,11 +26,24 @@ function MyApp ({ Component, pageProps, ...props }) {
     })
   }, [])
 
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const chatbox = document.getElementById('fb-customer-chat'); chatbox.setAttribute('page_id', '184672791387473'); chatbox.setAttribute('attribution', 'biz_inbox')
+      window.fbAsyncInit = function () { FB.init({ xfbml: true, version: 'v18.0' }) }; (function (d, s, id) { let js; const fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js'; fjs.parentNode.insertBefore(js, fjs) }(document, 'script', 'facebook-jssdk'))
+    }
+  }, [])
+
   return (
-    <AppProvider state={props}>
-      <Component {...{ ...pageProps, ...props }} />
-      <ToastContainer position="bottom-right"/>
-    </AppProvider>
+    <>
+
+      <AppProvider state={props}>
+        <Component {...{ ...pageProps, ...props }} />
+        <ToastContainer position="bottom-right"/>
+      </AppProvider>
+      <div id="fb-root"></div>
+      <div id="fb-customer-chat" className="fb-customerchat"></div>
+    </>
+
   )
 }
 
